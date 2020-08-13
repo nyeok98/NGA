bbcb3ae9db78d5ec9c3c7176843ef5cfb1565cc7
yuna0102 <mkiyg@naver.com>
2020-08-13 14:56:15 +0900

    [add] Blog models modified

--- NGA/main/models.py
+++ NGA/main/models.py
@@ -6,9 +6,12 @@ from django.db import models
 class Blog(models.Model):
     request_place = models.CharField(max_length=20)
     meet_place = models.CharField(max_length=20)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('auto_now_add=True')
     detail_request = models.TextField()
     fees = models.TextField()
+    limited_time_hour = models.IntegerField('number', default=0)
+    limited_time_min = models.IntegerField('number', default=0)
+    limited_time_sec = models.IntegerField('number', default=0)
 
     def __str__(self):
         return self.request_place
