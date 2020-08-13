8385d48485185367261c32d5c76b99c91e2af97e
kdh4387 <kdh4387@naver.com>
2020-08-13 17:28:45 +0900

    [add] added html to mypageApp

--- NGA/mypage/views.py
+++ NGA/mypage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def mypage(request):
     return render(request, 'mypage.html')
+
+def profile_update(request):
+    return render(request, 'profile_update.html')
