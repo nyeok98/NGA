30cb0d15a1add90c4a21b494f8fc0a4e67eb8c35
yuna0102 <mkiyg@naver.com>
2020-08-13 17:27:16 +0900

    [add] new [update] setting view url

--- NGA/NGA/urls.py
+++ NGA/NGA/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main, name="main"),
     path('mypage/', include('mypage.urls')),
-
+    path('new/', views.new, name="new"),
 ]
--- NGA/main/views.py
+++ NGA/main/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .models import Blog
 
 # Create your views here.
+
+
 def main(request):
     blogs = Blog.objects.all()
-    return render(request, 'main.html', {'blogs':blogs})
+    return render(request, 'main.html', {'blogs': blogs})
+
+
+def new(request):
+    blogs = Blog.objects.all()
+    return render(request, 'new.html', {'blogs': blogs})
