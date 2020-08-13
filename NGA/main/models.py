bae63701351f66cf160ea55af8fb328a20edfd4e
yuna0102 <mkiyg@naver.com>
2020-08-13 14:38:09 +0900

    [add] Blog model added

--- NGA/main/models.py
+++ NGA/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    request_place = models.CharField(max_length=20)
+    meet_place = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('date published')
+    detail_request = models.TextField()
+    fees = models.TextField()
+
+    def __str__(self):
+        return self.request_place
